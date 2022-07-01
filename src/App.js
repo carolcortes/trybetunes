39a5fea501e83af142f1afa849456c214c7c1245
carolcortes <caroline.ocortes@gmail.com>
2022-07-01 16:15:58 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import Search from './components/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!</p>);
+    return (
+      <div>
+        <p>TrybeTunes!</p>
+        <Switch>
+          <Route path="/" exact component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" exact component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </div>
+    );
   }
 }
 
