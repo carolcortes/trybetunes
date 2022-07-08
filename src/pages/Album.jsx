6cf4519ff8862402f3cc38ee9d55d02b51c17dc6
carolcortes <caroline.ocortes@gmail.com>
2022-07-08 11:57:58 -0300

    requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,17 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      musicList: '',
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getMusicList();
+  }
+
+  getMusicList = async () => {
+    const { match: { params: { id } } } = this.props;
+    const musicList = await getMusics(id);
+    this.setState({ musicList, loading: false });
+    // console.log(this.state.musicList);
+  }
+
   render() {
+    const { musicList, loading } = this.state;
     return (
       <div data-testid="page-album">
-        <h1>
-          Album
-          {/* {' '}
-          {this.props.match.params.id} */}
-        </h1>
+        { loading ? <p>Carregando...</p>
+          : (
+            <div>
+              <h1 data-testid="album-name">{musicList[0].collectionName}</h1>
+              <h2 data-testid="artist-name">{musicList[0].artistName}</h2>
+              { musicList.filter((music) => music.kind === 'song')
+                .map(({ trackId, trackName, previewUrl }) => (
+                  <MusicCard
+                    key={ trackId }
+                    trackName={ trackName }
+                    previewUrl={ previewUrl }
+                  />
+                )) }
+            </div>
+          )}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
 
 export default Album;
