efc4da78e415b3ae88e99cc6e0253d0c4d702b1b
carolcortes <caroline.ocortes@gmail.com>
2022-07-12 09:25:59 -0300

    requisito 12 parcial

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      favoritesList: [],
+    };
+  }
+
+  componentDidMount() {
+    if (!this.unmonted) this.getFavoritesList();
+  }
+
+  componentDidUpdate() {
+    if (!this.unmonted) this.getFavoritesList();
+  }
+
+  componentWillUnmount() {
+    this.unmonted = true;
+  }
+
+  getFavoritesList = async () => {
+    const favoriteData = await getFavoriteSongs();
+    this.setState({ favoritesList: favoriteData });
+  }
+
   render() {
+    const { favoritesList } = this.state;
     return (
       <div data-testid="page-favorites">
-        <h1>Favorites</h1>
+        <h1>Músicas Favoritas:</h1>
+        { favoritesList.map(({ trackName, previewUrl, trackId }) => (
+          <MusicCard
+            key={ trackId }
+            trackName={ trackName }
+            previewUrl={ previewUrl }
+            trackId={ trackId }
+            favoritesList={ favoritesList }
+          />
+        )) }
       </div>
     );
   }
