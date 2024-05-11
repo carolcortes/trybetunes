ecbe9d1684d2a7bbc8963762e9eca0a3067f3f31
carol cortes <caroline.cortes@dxnet.io>
2024-05-11 15:38:09 +0100

    feat: not found page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Content from './components/Content';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
           <Content />
         </Switch>
       </div>
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import Album from '../pages/Album';
 import Favorites from '../pages/Favorites';
-import NotFound from '../pages/NotFound';
 import Profile from '../pages/Profile';
 import ProfileEdit from '../pages/ProfileEdit';
 import Search from '../pages/Search';
@@ -20,7 +19,6 @@ class Content extends React.Component {
             <Route path="/favorites" component={ Favorites } />
             <Route exact path="/profile" component={ Profile } />
             <Route path="/profile/edit" component={ ProfileEdit } />
-            <Route path="*" component={ NotFound } />
           </Switch>
         </main>
       </>
